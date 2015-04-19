bf1fe902693d07c34d31ac125721675a1688f6c7
Neodyblue <sieramsn@gmail.com>
2015-04-19 17:32:35 +0200

    Added input file support

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 #include "disassembler.hh"
 
 int main(int argc, char *argv[])
 {
-  disassemble(std::cin, std::cout);
+  if (argc > 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " [<input>]" << std::endl;
+    return 1;
+  }
+
+  std::ifstream input;
+  if (argc == 2)
+  {
+    input.open(argv[1]);
+
+    if (!input)
+    {
+      std::cerr << "Cannot open file: '" << argv[1] << "'" << std::endl;
+      return 1;
+    }
+  }
+
+  disassemble(argc == 2 ? input : std::cin, std::cout);
   return 0;
 }
